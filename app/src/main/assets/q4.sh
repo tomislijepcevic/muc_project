abda38da3733a2b3ee399d5e7a93ec3223942410
tipytop <tipytop@gmail.com>
2015-11-10 01:55:03 +0100

    Created Android project. In assets folder there's bash script for capturing in monitor mode, and oui.csv file for mapping between mac and manufacturer.

---
+++ app/src/main/assets/q4.sh
@@ -0,0 +1,117 @@
+#!/system/bin/sh
+
+# ---------------------------
+# | CONFIGURATION VARIABLES |
+# ---------------------------
+TIME_FOR_BROAD_CAPTURE=${1-10}
+TIME_FOR_NARROW_CAPTURE=${2-15}
+FILE_OUI_MAPPING="oui.csv"
+
+# ------------------------
+# | FUNCTION DEFINITIONS |
+# ------------------------
+col_in_line() {
+  line=$1
+  column=$2
+  echo $line | cut -d, -f$column
+}
+
+oui_lookup() {
+  mac=$1
+  mac_oui=$(echo $mac | cut -d: -f1,2,3)
+  mac_nic=$(echo $mac | cut -d: -f4,5,6)
+  mac_oui_resolved=$(grep -i $mac_oui oui.csv | cut -d, -f2)
+  echo $mac_oui_resolved
+}
+
+print_client() {
+  line=$1
+  mac=$(col_in_line "$line" 1)
+  pwr=$(col_in_line "$line" 4)
+  bssid=$(col_in_line "$line" 6)
+  mac_oui_resolved=$(oui_lookup $mac)
+  echo $pwr,$mac,$mac_oui_resolved,$bssid
+}
+
+# -------------
+# | MAIN CODE |
+# -------------
+# Start broad capture
+file_broad_capture="broad-01.csv"
+
+if [ -z $file_broad_capture ]; then
+  rm $file_broad_capture
+fi
+
+/system/xbin/airodump-ng -w broad --output-format csv eth0 2>/dev/null &
+sleep $TIME_FOR_BROAD_CAPTURE
+killall -INT airodump-ng
+wait
+sleep 2
+
+# Process broad capture
+dividing_line=$(grep -n "^Station" $file_broad_capture | cut -d: -f1)
+num_of_lines=$(wc -l $file_broad_capture | cut -d' ' -f 1)
+ap_from_line=2
+ap_to_line=`expr $dividing_line - 2`
+cl_from_line=$dividing_line
+cl_to_line=`expr $num_of_lines - 1`
+
+file_tmp="tmp.csv"
+> $file_tmp
+i=0
+
+cat $file_broad_capture | while read line; do
+  if [ $i -ge $ap_from_line -a $i -lt $ap_to_line ]; then
+    mac=$(col_in_line "$line" 1)
+    ch=$(col_in_line "$line" 4)
+    pwr=$(col_in_line "$line" 9)
+
+    echo $pwr,$mac,$ch >> $file_tmp
+  elif [ $i -ge $cl_from_line -a $i -lt $cl_to_line ]; then
+    print_client $line
+  fi
+
+  i=`expr $i + 1` 
+done
+
+# Sort AP in broad capture by power
+cat $file_tmp > $file_broad_capture
+sort -nr $file_broad_capture > $file_tmp
+cat $file_tmp > $file_broad_capture
+
+# Start narrow capture
+file_narrow_capture="narrow-01.csv"
+
+if [ -z $file_narrow_capture ]; then
+  rm $file_narrow_capture
+fi
+
+cat $file_broad_capture | while read line; do
+  mac=$(col_in_line "$line" 2)
+  ch=$(col_in_line "$line" 3)
+
+  /system/xbin/airodump-ng -w narrow --output-format csv -c $ch --bssid $mac eth0 2>/dev/null &
+  sleep $TIME_FOR_NARROW_CAPTURE
+  killall -INT airodump-ng
+  wait
+
+  dividing_line=$(grep -n "^Station" $file_narrow_capture | cut -d: -f1)
+  num_of_lines=$(wc -l $file_narrow_capture | cut -d' ' -f 1)
+  cl_from_line=$dividing_line
+  cl_to_line=`expr $num_of_lines - 1`
+  i=0
+
+  cat $file_narrow_capture | while read _line; do
+    if [ $i -ge $cl_from_line -a $i -lt $cl_to_line ]; then
+      print_client $_line
+    fi
+
+    i=`expr $i + 1` 
+  done
+
+  rm $file_narrow_capture
+done
+
+rm $file_tmp
+rm $file_broad_capture
---
+++ app/src/main/java/muc/project/MainActivity.java
@@ -0,0 +1,82 @@
+package muc.project;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final String AIRODUMP_UTIL_SCRIPT = "q4.sh";
+    private static final String OUI_CSV = "oui.csv";
+    private static final int AIRODUMP_BROAD_CAPTURE_DURATION = 5;
+    private static final int AIRODUMP_NARROW_CAPTURE_DURATION = 10;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        try {
+            makeNewFile(getAssets().open(OUI_CSV), OUI_CSV);
+            File script = makeNewFile(getAssets().open(AIRODUMP_UTIL_SCRIPT), AIRODUMP_UTIL_SCRIPT);
+            execScriptAsRoot(script);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void execScriptAsRoot(File script) {
+        try {
+            final Runtime runtime = Runtime.getRuntime();
+            File dir = script.getParentFile();
+
+            runtime.exec("su");
+            runtime.exec("mount -o remount,rw /system ");
+            Process p = runtime.exec(script.getAbsolutePath(), null, dir);
+
+            Log.d(TAG, "Start");
+            String line;
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            while ((line = input.readLine()) != null) {
+                textView.setText("result : " + line);
+                Log.d(TAG, line);
+            }
+
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private File makeNewFile(InputStream is, String filename) throws IOException {
+        File file = new File (getFilesDir(), filename);
+        FileOutputStream os = new FileOutputStream(file);
+
+        file.setExecutable(true, false);
+        file.setWritable(true, false);
+        file.setReadable(true, false);
+
+        int read = 0;
+        byte[] bytes = new byte[1024];
+
+        while ((read = is.read(bytes)) != -1)
+            os.write(bytes, 0, read);
+
+        is.close();
+        os.close();
+
+        return file;
+    }
+}
