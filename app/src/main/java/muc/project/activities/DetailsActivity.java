dab570918d1c86ff0fa6a47779ab6f01bf10ea3e
Peter Us <peter.us.99@gmail.com>
2016-01-12 12:02:56 +0100

    Working on details activity.

--- app/src/main/java/muc/project/activities/DetailsActivity.java
+++ app/src/main/java/muc/project/activities/DetailsActivity.java
@@ -2,6 +2,8 @@ package muc.project.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -14,12 +16,49 @@ import muc.project.R;
 public class DetailsActivity extends FragmentActivity {
     private GoogleMap mMap;
 
+    private TextView nameLabel;
+    private TextView manufacturerLabel;
+    private TextView counterLabel;
+    private TextView lastSeenLabel;
+    private TextView lastLocationLabel;
+    private TextView previousLocationsLabel;
+
+    private EditText name;
+    private TextView manufacturer;
+    private TextView counter;
+    private TextView lastSeen;
+    private TextView lastLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.device_details);
         setUpMapIfNeeded();
+        initFields();
+    }
 
+    private void initFields() {
+        nameLabel = (TextView) findViewById(R.id.name_label);
+        manufacturerLabel = (TextView) findViewById(R.id.manufacturer_label);
+        counterLabel = (TextView) findViewById(R.id.counter_label);
+        lastSeenLabel = (TextView) findViewById(R.id.last_seen_label);
+        lastLocationLabel = (TextView) findViewById(R.id.last_location_label);
+        previousLocationsLabel = (TextView) findViewById(R.id.previous_locations_label);
+        name = (EditText) findViewById(R.id.name);
+        manufacturer = (TextView) findViewById(R.id.manufacturer);
+        counter = (TextView) findViewById(R.id.counter);
+        lastSeen = (TextView) findViewById(R.id.last_seen);
+        lastLocation = (TextView) findViewById(R.id.last_location);
+
+        // Append ":" to all the labels.
+        nameLabel.append(":");
+        manufacturerLabel.append(":");
+        counterLabel.append(":");
+        lastSeenLabel.append(":");
+        lastLocationLabel.append(":");
+        previousLocationsLabel.append(":");
+
+        // TODO: Set name,manufactuter,counter,lastSeen,lastLocation, based on current selected device.
     }
 
     @Override
@@ -27,8 +66,6 @@ public class DetailsActivity extends FragmentActivity {
         super.onResume();
         setUpMapIfNeeded();
     }
-
-
 
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
@@ -38,9 +75,13 @@ public class DetailsActivity extends FragmentActivity {
                     .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
-                //setUpMap();
+                setUpMap();
             }
         }
+    }
+
+    private void setUpMap() {
+        // TODO add markers and stuff.
     }
 }
 
